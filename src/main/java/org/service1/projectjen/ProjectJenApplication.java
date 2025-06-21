52bc211929aa6b7a27d2a0a5417d681591eef07e
Pathan Aftaab Khan <pathanaftaabkhan>
2025-06-21 14:19:05 +0530

    pilot

---
+++ src/main/java/org/service1/projectjen/ProjectJenApplication.java
@@ -0,0 +1,13 @@
+package org.service1.projectjen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/service1/projectjen/ProjectJenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.service1.projectjen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
